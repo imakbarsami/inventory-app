7ac1f4ff86a5a58e0270d0cc6405659e8532ddb9
Akbar Sami <imakbarsami@gmail.com>
2026-02-24 02:06:27 +0600

    create product route added

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -4,7 +4,7 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{ __('Products Management') }}
             </h2>
-            <a href="#" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
+            <a href="{{ route('products.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
                 + Add New Product
             </a>
         </div>
