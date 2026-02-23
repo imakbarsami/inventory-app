e3c774dec1affb65b80c9fdc10cb835618ffd3e4
Akbar Sami <imakbarsami@gmail.com>
2026-02-23 19:33:29 +0600

    set up project & database

---
+++ database/migrations/2026_02_23_132221_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained()->cascadeOnDelete(); 
+            $table->string('invoice_no')->unique(); 
+            $table->date('sale_date'); 
+            
+            $table->decimal('gross_amount', 10, 2); 
+            $table->decimal('discount', 10, 2)->default(0); 
+            $table->decimal('vat', 10, 2)->default(0); 
+            $table->decimal('net_amount', 10, 2);
+            $table->decimal('paid_amount', 10, 2)->default(0); 
+            $table->decimal('due_amount', 10, 2)->default(0); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
